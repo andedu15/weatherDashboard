0d8c9c41edb021ab89b91c72667fb9e5ed4c63ce
Andres Mejia <mejiandrese15@gmail.com>
2023-10-02 18:06:06 -0600

    history list

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,9 @@ var date = document.querySelector('.date');
 
 
 
-
 button.addEventListener('click', currentWeather)
 
-
+// CURRENT WEATHER
 function currentWeather(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
 .then(response => response.json())
@@ -22,25 +21,56 @@ function currentWeather(){
         var tempValue = data['main']['temp'];
         var windValue = data['wind']['speed'];
         var humidityValue = data['main']['humidity'];
+        var id = data['id'];
+        var history = [];
 
         city.innerHTML = nameValue;
-        temp.innerHTML = "Temp: "+tempValue+" °";
+        temp.innerHTML = "Temp: "+toFahrenheit(tempValue)+" °F";
         wind.innerHTML = "Wind: "+windValue+" MPH";
         humidity.innerHTML = "Humidity: "+humidityValue+" %";
         date.innerHTML = dayjs().format('DD/MM/YYYY');
+        
+
+
+        var search = {
+            city: nameValue,
+            id: id,
+        }
+
+        localStorage.setItem("Search", JSON.stringify(search)); //Save in local storage
+        history.push(search.city);
+
+        // SHOW HISTORY
+        var list = document.createElement('button');
+        list.appendChild(document.createTextNode(history));
+        document.querySelector('ul').appendChild(list);
 
 
   
   console.log(data);
+  console.log(history);
+  
 
 })
+
 
 .catch(err => alert("Wrong city name!"));
 }
 
+function toFahrenheit(tempValue){
+    var fahrenheit = Math.trunc((tempValue-273)*(9/5)+32);
+    return fahrenheit
+}
+
+console.log(history);
+
 function forecastWeather(){
 
 }
+
+
+
+
 
 
 
